a51e93781fb68deb3e14b31876474ef189913d12
xx <110752841+igasome@users.noreply.github.com>
2024-02-23 23:59:04 +0000

    hj

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ console.warn(
   "Feel free to use anything you find here for your projects; credit is appreciated but not required! Visit my website at https://3kh0.net for more information."
 );
 
-const local_title = localStorage.getItem("title");
-const local_icon = localStorage.getItem("icon");
-if (window.localStorage.hasOwnProperty("title")) {
+const local_title = localStorage.getItem("Bored");
+const local_icon = localStorage.getItem("#bored.svg");
+if (window.localStorage.hasOwnProperty("Bored")) {
   document.title = local_title;
   console.log("Title set to: " + local_title);
 }
